95762924c9a710ac6916a23466da75e96491512f
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-07-23 01:26:31 +0500

    date and time added

--- modules/dateandtime.js
+++ modules/dateandtime.js
@@ -1,9 +1,9 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
 export function dateandtime() {
-  const displayDate = document.getElementById('displayDate');
+  const date = document.getElementById('dateTime');
   const dt = DateTime.now();
-  displayDate.innerHTML = `${dt}`;
+  date.innerHTML = `${dt}`;
 }
 
 setInterval(dateandtime, 1000);
